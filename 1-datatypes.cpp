25628846b6167b5084ec386b9bda5f3806f810be
Zeeshan Ali <wazirzeeshan531@gmail.com>
2026-09-12 17:01:54 +0500

    add further datatypes

--- 1-datatypes.cpp
+++ 1-datatypes.cpp
@@ -6,17 +6,20 @@ int main()
 {
     // INTEGER DATA TYPES
 
-    int age = 20;                   // integer data type
-    short int shortAge = 20;        // int data type but shorter in size than int
-    long int longAge = 20;          // int data type but longer in size than int
-    long long int longLongAge = 20; // int data type but longer in size than long int
+    int age = 20;                   // int data type
+    short int shortAge = 20;        // shorter size than int (typically 2 bytes) range: -32,768 to 32,767
+    long int longAge = 20;          // longer size than int (typically 4 bytes) range: -2,147,483,648 to 2,147,483,647
+    long long int longLongAge = 20; // longer size than long int (typically 8 bytes) range: -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+
     // signed int
     int temperature = -10; // signed int --> stores both positive and negative values
     // unsigned int
     unsigned int positiveTemperature = 10; // unsigned int --> stores only positive values
 
-    float height = 5.9; // float data type
-    double weight = 70.5; // double data type
+    // FLOATING-POINT DATA TYPES
+
+    float height = 5.9;   // float --> for 6-7 digit precision (size: 4 bytes)
+    double weight = 70.5; // double --> for 15-16 digit precision (size: 8 bytes)
 
     char grade = 'A'; // character data type
 
