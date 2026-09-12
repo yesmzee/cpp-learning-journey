cc5ec81e1fc528f95fbf92bfab925808b53b9069
Zeeshan Ali <wazirzeeshan531@gmail.com>
2026-09-12 16:19:17 +0500

    add DATA-TYPES

---
+++ 1-datatypes.cpp
@@ -0,0 +1,18 @@
+// DATA-TYPES in CPP
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int age = 20; // integer data type
+
+    float height = 5.9; // float data type
+
+    double weight = 70.5; // double data type
+
+    char grade = 'A'; // character data type
+
+    bool isStudent = true; // boolean data type
+
+    string name = "Zeeshan"; // string data type
+}
