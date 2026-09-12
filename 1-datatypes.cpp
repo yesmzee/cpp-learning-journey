78e30f65159db3d2f85b251e241546de7a284ced
Zeeshan Ali <wazirzeeshan531@gmail.com>
2026-09-12 16:46:49 +0500

    add more explanation

--- 1-datatypes.cpp
+++ 1-datatypes.cpp
@@ -4,10 +4,18 @@
 using namespace std;
 int main()
 {
-    int age = 20; // integer data type
+    // INTEGER DATA TYPES
+
+    int age = 20;                   // integer data type
+    short int shortAge = 20;        // int data type but shorter in size than int
+    long int longAge = 20;          // int data type but longer in size than int
+    long long int longLongAge = 20; // int data type but longer in size than long int
+    // signed int
+    int temperature = -10; // signed int --> stores both positive and negative values
+    // unsigned int
+    unsigned int positiveTemperature = 10; // unsigned int --> stores only positive values
 
     float height = 5.9; // float data type
-
     double weight = 70.5; // double data type
 
     char grade = 'A'; // character data type
