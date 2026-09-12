4639f757001f6eab4bcf56b347d8f38b4b01d32a
Zeeshan Ali <wazirzeeshan531@gmail.com>
2026-09-12 14:54:52 +0500

    modify first.cpp

---
+++ first.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main()
+{
+    std::cout << "Hello, World !" << std::endl;
+    std::cout << "this is zeeshan's first cpp file " << std::endl
+              << "learning the basics again";
+}
