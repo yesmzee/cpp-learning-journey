3744c12881869e735015cc1deee3c984f723f0a1
Zeeshan Ali <wazirzeeshan531@gmail.com>
2026-09-12 15:02:07 +0500

    add using-namespace-std

--- first.cpp
+++ first.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
+using namespace std;
 int main()
 {
-    std::cout << "Hello, World !" << std::endl;
-    std::cout << "this is zeeshan's first cpp file " << std::endl
+    cout << "Hello, World !" << endl;
+    cout << "this is zeeshan's first cpp file " << endl
               << "learning the basics again";
 }
