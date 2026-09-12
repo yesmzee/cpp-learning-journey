27b3ee23015d6e9e1aab2cebf09e8b78ace6a279
Zeeshan Ali <wazirzeeshan531@gmail.com>
2026-09-12 15:19:06 +0500

    add -about comments

--- first.cpp
+++ first.cpp
@@ -2,7 +2,14 @@
 using namespace std;
 int main()
 {
+    // using cout 
     cout << "Hello, World !" << endl;
     cout << "this is zeeshan's first cpp file " << endl
               << "learning the basics again";
+    // - about comments :
+
+    // this is commment (single line comment)
+
+    /* cout << "this text is ignored by compiler bcuz it is a multi-line comment" */
+    
 }
