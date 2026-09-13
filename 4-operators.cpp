5dcb90de95d481f306cc70ec045bb90cc6b4c909
Zeeshan Ali <wazirzeeshan531@gmail.com>
2026-09-13 09:01:48 +0500

    add arithmetic operators

---
+++ 4-operators.cpp
@@ -0,0 +1,20 @@
+// OPERATORS IN CPP
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // there are different types of operators in cpp :
+
+    // arithmetic operators ( +, -, *, /, % ) used for mathematical calculations
+
+    int a = 10, b = 5;
+    cout << a + b << endl;  // addition
+    cout << a - b << endl;  // subtraction
+    cout << a * b << endl;  // multiplication
+    cout << a / b << endl;  // division
+    cout << 10 % 3 << endl; // modulus --> gives remainder of division
+
+    return 0;
+}
