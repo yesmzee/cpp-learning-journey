dd1774cd928ac6f128563fab3659b99de07ded13
Zeeshan Ali <wazirzeeshan531@gmail.com>
2026-09-13 09:04:45 +0500

    add assignment operators

--- 4-operators.cpp
+++ 4-operators.cpp
@@ -16,5 +16,23 @@ int main()
     cout << a / b << endl;  // division
     cout << 10 % 3 << endl; // modulus --> gives remainder of division
 
+
+    // assignment operators ( =, +=, -=, *=, /=, %= ) used to assign or update values
+
+    int c = 10; // simple assignment
+
+    // compound assignment operators ( +=, -=, *=, /=, %= )
+    c += 5; // c = c + 5
+    cout << c << endl;
+    c -= 3; // c = c - 3
+    cout << c << endl;
+    c *= 2; // c = c * 2
+    cout << c << endl;
+    c /= 4; // c = c / 4
+    cout << c << endl;
+    c %= 3; // c = c % 3
+    cout << c << endl;
+    
+
     return 0;
 }
