3e513c95762b5a499d6578cbe4d3ca4faac1e241
Zeeshan Ali <wazirzeeshan531@gmail.com>
2026-09-13 09:25:45 +0500

    add increment & decreament operators

--- 4-operators.cpp
+++ 4-operators.cpp
@@ -16,7 +16,6 @@ int main()
     cout << a / b << endl;  // division
     cout << 10 % 3 << endl; // modulus --> gives remainder of division
 
-
     // assignment operators ( =, +=, -=, *=, /=, %= ) used to assign or update values
 
     int c = 10; // simple assignment
@@ -34,8 +33,8 @@ int main()
     c %= 3; // c = c % 3
     cout << c << endl;
 
-    // comparison operators ( ==, !=, >, <, >=, <= ) used to compare two values 
-    
+    // comparison operators ( ==, !=, >, <, >=, <= ) used to compare two values
+
     int x = 10, y = 20;
 
     cout << (x == y) << endl; // equal to
@@ -46,12 +45,22 @@ int main()
     cout << (x <= y) << endl; // less than or equal to
 
     // logical operators ( &&, ||, ! ) used to combine conditions in conditional statements
-    
+
     int x = 10, y = 20, z = 30;
     cout << (x < y && y < z) << endl; // if both conditions are true then it will return true (1)
     cout << (x < y || y > z) << endl; // if any one condition is true then it will return true (1)
-    cout << !(x > y) << endl;          // if the condition is false then it will return true (1)
-    
+    cout << !(x > y) << endl;         // if the condition is false then it will return true (1) & vice versa
+
+    // increment and decrement operators ( ++, -- ) used to increase or decrease the value of a variable by 1
+
+    int a = 10, b = 20;
+    cout << a++ << endl;
+    // post-increment --> first it will print the value of a and then it will increase the value of a by 1
+    cout << ++b << endl;
+    // pre-increment --> first it will increase the value of b by 1 and then it will print the value of b
+
+    cout << a-- << endl; // post-decrement
+    cout << --b << endl; // pre-decrement
 
     return 0;
 }
