ef392f7508ac64ea84370b7f5f130257eda57f88
Zeeshan Ali <wazirzeeshan531@gmail.com>
2026-09-18 07:26:38 +0500

    feat: add comments

--- 06-swtich-statement.cpp
+++ 06-swtich-statement.cpp
@@ -11,10 +11,10 @@ int main()
 
     switch (age > 18 && age < 25)
     {
-    case true:
+    case true: // can add multiple cases like case 1: case 2: case 3: etc.
         cout << "adult\nyou can vote";
         break;
-    default:
+    default: // executes when none of the cases are true
         cout << "meri jaan you're under 18 \nyou can not vote";
         break;
     }
